4db444bb97062d193ee97e3ba0a8442bd9aded95
Andrey <ssa25t@mail.ru>
2026-02-16 23:05:18 +0500

    added API

--- src/backend/smarthouse/devices/views.py
+++ src/backend/smarthouse/devices/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema, OpenApiResponse, inline_serializer
 
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.http import JsonResponse
 from .models import Device
+from .serializers import message, heartbeat
 # Register your models here.
 
+
+
+@extend_schema(
+    summary='heartbeat устройства',
+    description='Обновляет heartbeat устройства по имени',
+    tags=['Devices'],
+    request= heartbeat,
+    responses={
+        200: message,
+        404: message
+    }
+)
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def heartbeat(request, name):
@@ -15,10 +29,9 @@ def heartbeat(request, name):
     device = Device.objects.filter(name=name, token=token).first()
 
     if not device:
-        return JsonResponse({'error': 'Device not found'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'message': 'Device not found'}, status=status.HTTP_404_NOT_FOUND)
 
     device.update_last_heartbeat()
-
 
     return JsonResponse({"message": "OK"}, status=status.HTTP_200_OK)
 
--- src/backend/smarthouse/smarthouse/settings.py
+++ src/backend/smarthouse/smarthouse/settings.py
@@ -54,7 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'devices'
+    'devices',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,16 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Device API',
+    'DESCRIPTION': 'API для работы с датчиками и микроконтроллерами',
+    'VERSION': '1.0.0',
+}
 
 ROOT_URLCONF = 'smarthouse.urls'
 
